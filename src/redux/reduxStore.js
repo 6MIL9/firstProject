afa8bab75a9000007ca28d7cf2066596d81ecfb0
6MIL9 <emilkliavlin2@gmail.com>
2020-06-01 18:16:32 +0500

    redux devtools

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
 import profileReducer from './profileReducer';
 import dialogsReducer from './dialogsReducer';
 import sidebarReducer from './sidebarReducer';
@@ -7,7 +7,6 @@ import authReducer from './authReducer';
 import thunkMiddleware from 'redux-thunk';
 import appReducer from './appReducer';
 import { reducer as formReducer } from 'redux-form';
-
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -19,8 +18,7 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
-
-window.store = store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export default store;
