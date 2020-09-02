044921f7bc83ad4b0f18f887cb9039dc027656d4
6MIL9 <emilkliavlin2@gmail.com>
2020-09-02 22:11:26 +0500

    adaptive cm 2 (menu)

---
+++ src/components/Header/HamburgerMenu/SideMenu.js
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react';
+import styled, { css } from 'styled-components';
+import { MenuContext } from '../../../context/navState';
+import { NavLink } from 'react-router-dom';
+import classes from './SideMenu.module.css';
+
+const Menu = styled.nav`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  bottom: 0px;
+  z-index: 293;
+  display: block;
+  width: 220px;
+  margin-top: 0px;
+  padding-top: 4rem;
+  padding-right: 0px;
+  align-items: stretch;
+  background-color: #00a3ff;
+  transform: translateX(-100%);
+  transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  ${props =>
+    props.open &&
+    css`
+      transform: translateX(0);
+    `}
+`;
+
+
+export const SideMenu = ({ children }) => {
+  const { isMenuOpen } = useContext(MenuContext);
+
+  return <Menu open={isMenuOpen}>{children}</Menu>;
+};
+
+
+
+SideMenu.defaultProps = {
+  children: (
+      <nav className={classes.nav}>
+        <div className={classes.item}>
+          <NavLink to="/profile" className={classes.link} activeClassName={classes.activeLink}>Profile</NavLink>
+        </div>
+
+        <div className={classes.item}>
+          <NavLink to="/dialogs" className={classes.link} activeClassName={classes.activeLink}>Messages</NavLink>
+        </div>
+
+        <div className={classes.item}>
+          <NavLink to="/Users" className={classes.link} activeClassName={classes.activeLink}>Users</NavLink>
+        </div>
+
+        <div className={classes.item}>
+          <NavLink to="/Settings" className={classes.link} activeClassName={classes.activeLink}>Settings</NavLink>
+        </div>
+      </nav>
+  ),
+};
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import classes from './Header.module.css';
 import { NavLink } from 'react-router-dom';
+import NavState from '../../context/navState';
+import MainMenu from './HamburgerMenu/MainMenu';
 
 let Header = (props) => {
   return (
     <header className={classes.header}>
-      <div className={classes.block}>block</div>
+      <div className={classes.block}>
+        <NavState>
+          <MainMenu />
+        </NavState>
+      </div>
       <div className={classes.loginContainer}>
         {props.isAuth ? <div> <span className={classes.userName}>{props.login}</span> -
         <button className={classes.btn} onClick={props.logout}>Logout</button></div> : <NavLink to='/login'>Login</NavLink>}
