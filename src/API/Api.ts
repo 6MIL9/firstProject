c7f3214b937744640ab821ecee9bd833a69cbea8
6MIL9 <emilkliavlin2@gmail.com>
2020-12-06 23:00:00 +0500

    Api on TS

--- src/API/Api.ts
+++ src/API/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { ProfileType } from '../Types/Types';
-//пока не знаю как
+
 const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
@@ -13,31 +13,31 @@ export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 5) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
     },
-    follow(userId) {
+    follow(userId: number) {
         return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
     },
-    unfollow(userId) {
+    unfollow(userId: number) {
         return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
     }
 };
 
 export const userProfileAPI = {
-    getUserProfile(userId) {
+    getUserProfile(userId: number) {
         return instance.get(`profile/${userId}`)
     },
-    getStatus(userId) {
+    getStatus(userId: number) {
         return instance.get(`profile/status/${userId}`)
     },
-    updateStatus(status) {
+    updateStatus(status: string) {
         return instance.put(`profile/status`, { status: status })
     },
-    savePhoto(photo) { 
+    savePhoto(photo: any) { 
         let formData = new FormData();
         formData.append('image', photo)
 
         return instance.put(`profile/photo`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
     },
-    saveProfile(profile) {
+    saveProfile(profile: ProfileType) {
         return instance.put(`profile`, profile)
     }
 };
@@ -46,7 +46,7 @@ export const authAPI = {
     getAuthUserData() {
         return instance.get(`auth/me`);
     },
-    sentLoginData(email, password, rememberMe, captcha) {
+    sentLoginData(email: string, password: string, rememberMe: boolean, captcha: null | string = null) {
         return instance.post('/auth/login', {
             email, password, rememberMe, captcha
         });
@@ -61,4 +61,5 @@ export const securityAPI = {
         return instance.get('/security/get-captcha-url');
     }
 };
+
 
