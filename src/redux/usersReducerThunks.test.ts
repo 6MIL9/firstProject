9394a791d48d891e699941b76ac07838e4ac5bc6
6MIL9 <emilkliavlin2@gmail.com>
2021-02-05 17:17:34 +0500

    unfollow thunk test

--- src/redux/usersReducerThunks.test.ts
+++ src/redux/usersReducerThunks.test.ts
@@ -1,9 +1,9 @@
-import { actions, follow, } from "./usersReducer"
+import { actions, follow, unfollow, } from "./usersReducer"
 import { usersAPI } from './../API/usersApi';
 import { ResultCodes, ResponseType } from "../API/Api";
 
 jest.mock('./../API/usersApi')
-const usersAPIMock = usersAPI
+const usersAPIMock = usersAPI as jest.Mocked<typeof usersAPI>
 
 const result: ResponseType = {
    resultCode: ResultCodes.Success,
@@ -11,18 +11,42 @@ const result: ResponseType = {
    data: {},
 }
 
-//@ts-ignore
-usersAPIMock.follow.mockReturnValue(Promise.resolve(result))
+const dispatchMock = jest.fn();
+const getStateMock = jest.fn()
 
-test("", async () => {
+beforeEach(() => {
+   dispatchMock.mockClear()
+   getStateMock.mockClear()
+   usersAPIMock.follow.mockClear()
+   usersAPIMock.unfollow.mockClear()
+})
+
+usersAPIMock.follow.mockReturnValue(Promise.resolve(result))
+usersAPIMock.unfollow.mockReturnValue(Promise.resolve(result))
+
+test("success follow thunk", async () => {
 
    const thunk = follow(1)
-   const dispatchMock = jest.fn();
 
-   //@ts-ignore
-   await thunk(dispatchMock)
-
+   await thunk(dispatchMock, getStateMock, {})
 
    expect(dispatchMock).toBeCalledTimes(3)
+   expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, 1))
+   expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.followSuccess(1))
+   expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.toggleFollowingProgress(false, 1))
+
+})
+
+test("success unfollow thunk", async () => {
+
+   const thunk = unfollow(1)
+
+   await thunk(dispatchMock, getStateMock, {})
+
+   expect(dispatchMock).toBeCalledTimes(3)
+   expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, 1))
+   expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.unfollowSuccess(1))
+   expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.toggleFollowingProgress(false, 1))
+
 })
 
